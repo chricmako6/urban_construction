8e7bde6e8ba6bf02dd303cc7ab6bb699b6a43365
chricmako6 <156549010+chricmako6@users.noreply.github.com>
2026-02-11 02:14:15 -0500

    refactor pagination and shop session components for improved functionality and layout

--- component/03/pagination.jsx
+++ component/03/pagination.jsx
@@ -3,33 +3,35 @@ import React from "react";
 import { BiArrowToTop } from "react-icons/bi";
 
 function Pagination({ currentPage, totalPages, onPageChange }) {
+
   const pageNumbers = Array.from({ length: totalPages }, (_, i) => i + 1);
 
   return (
-    <span className="flex justify-center mx-auto gap-5 mt-4 w-33 border border-white rounded-full">
-      {/* Previous Button */}
+    <div className="flex justify-center gap-5 mt-4">
+
+      {/* Previous */}
       <button
         onClick={() => onPageChange(currentPage - 1)}
         disabled={currentPage === 1}
-        className={`p-3 rounded-full text-sm ${
+        className={`p-3 rounded-full ${
           currentPage === 1
             ? "bg-gray-200 text-gray-400 cursor-not-allowed"
-            : "bg-[#ffd061] text-gray-700 hover:bg-[#f5c84a] shadow-2xl cursor-pointer"
+            : "bg-[#ffd061] hover:bg-[#f5c84a] cursor-pointer"
         }`}
       >
         <BiArrowToTop className="w-5 h-5 -rotate-90"/>
       </button>
 
       {/* Page Numbers */}
-      <div className="flex space-x-2">
+      <div className="flex gap-2">
         {pageNumbers.map((number) => (
           <button
             key={number}
             onClick={() => onPageChange(number)}
-            className={`px-4 py-2 rounded-md text-sm ${
+            className={`px-4 py-2 rounded-md ${
               currentPage === number
-                ? "text-gray-200 bg-gray-800 cursor-pointer"
-                : "hover:bg-white text-gray-700 bg-gray-100 cursor-pointer"
+                ? "bg-gray-800 text-white"
+                : "bg-gray-100 hover:bg-white"
             }`}
           >
             {number}
@@ -37,20 +39,20 @@ function Pagination({ currentPage, totalPages, onPageChange }) {
         ))}
       </div>
 
-      {/* Next Button */}
+      {/* Next */}
       <button
-        // onClick={() => onPageChange(currentPage + 1)}
-        onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
+        onClick={() => onPageChange(currentPage + 1)}
         disabled={currentPage === totalPages}
-        className={`p-3 rounded-full text-sm ${
+        className={`p-3 rounded-full ${
           currentPage === totalPages
             ? "bg-gray-200 text-gray-400 cursor-not-allowed"
-            : "bg-[#ffd061] text-gray-700 hover:bg-[#f5c84a] shadow-2xl cursor-pointer"
+            : "bg-[#ffd061] hover:bg-[#f5c84a] cursor-pointer"
         }`}
       >
         <BiArrowToTop className="w-5 h-5 rotate-90"/>
       </button>
-    </span>
+
+    </div>
   );
 }
 
--- component/03/shopsession2.jsx
+++ component/03/shopsession2.jsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from 'react'
 import Link from 'next/link';
 import Pagination from '@/component/03/pagination';
@@ -9,7 +10,21 @@ import { PiMapPinAreaDuotone } from 'react-icons/pi'
 import { CiLineHeight } from "react-icons/ci";
 import { AiOutlineColumnWidth } from "react-icons/ai";
 
-function Shopsession2({}) {
+function Shopsession2() {
+
+  const itemsPerPage = 9; 
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(shopItems.length / itemsPerPage);
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+  const currentItems = shopItems.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
 
   return (
     <div>
@@ -21,68 +36,75 @@ function Shopsession2({}) {
       </div>
 
       <div className='w-full mt-5'>
-      <div className='grid grid-cols-3 justify-between gap-5 bg-white p-5 rounded-xl shadow'>
-       {shopItems.map((item) => (
-        <div key={item.id} className='bg-white rounded-xl shadow-md relative'>
-          <span className='absolute px-4 text-shadow-sm font-bold text-white left-40 top-56 p-2 rounded-full bg-[#ffd061] hover:bg-[#f5c84a] cursor-pointer'>
-            <Link href={`/shop/${item?.id}`}>+Quick Buy</Link>
-          </span>
-
-          <img
-            src={item.image}
-            alt={item.title}
-            className='h-70 w-full rounded-t-xl bg-gray-200 object-cover'
-          />
-
-          <div className='p-3'>
-            <h2 className='font-bold'>{item.title} {item.id}</h2>
-            <p className='flex justify-between'>
-              <span className='my-2'>From {item.price}</span>
-              <span className='flex gap-1'>
-                <FaRegStar className='w-5 h-5 cursor-pointer' />
-                <FaRegStar className='w-5 h-5 cursor-pointer' />
-                <FaRegStar className='w-5 h-5 cursor-pointer' />
+        <div className='grid grid-cols-3 gap-5 bg-white p-5 rounded-xl shadow'>
+          {currentItems.map((item) => (
+            <div key={item.id} className='bg-white rounded-xl shadow-md relative'>
+              
+              <span className='absolute px-4 font-bold text-white left-40 top-56 p-2 rounded-full bg-[#ffd061] hover:bg-[#f5c84a] cursor-pointer'>
+                <Link href={`/shop/${item?.id}`}>+Quick Buy</Link>
               </span>
-            </p>
-          </div>
-          <div className="grid grid-cols-3 p-2 gap-2">
-            <div className="mx-auto text-xs p-1  w-20">
-              <MdApartment className='w-6 h-6 mx-auto cursor-pointer'/>
-              <p className="text-center my-1.5">{item?.floors} Floors</p>
-            </div>
 
-            <div className="mx-auto text-xs p-1  w-20 border-r border-l border-gray-400">
-              <MdBedroomChild className='w-6 h-6 mx-auto cursor-pointer'/>
-              <p className="text-center my-1.5">{item?.bedroom} Bedrooms</p>
-            </div>
+              <img
+                src={item.image}
+                alt={item.title}
+                className='h-70 w-full rounded-t-xl bg-gray-200 object-cover'
+              />
 
-            <div className="mx-auto text-xs p-1  w-20">
-              <MdOutlineBathroom className='w-6 h-6 mx-auto cursor-pointer'/>
-              <p className="text-center my-1.5">{item?.bathroom} Bathrooms</p>
-            </div>
+              <div className='p-3'>
+                <h2 className='font-bold'>{item.title}</h2>
+                <p className='flex justify-between'>
+                  <span className='my-2'>From {item.price}</span>
+                  <span className='flex gap-1'>
+                    <FaRegStar className='w-5 h-5'/>
+                    <FaRegStar className='w-5 h-5'/>
+                    <FaRegStar className='w-5 h-5'/>
+                  </span>
+                </p>
+              </div>
 
-             <div className="mx-auto w-20 text-xs p-1 ">
-              <CiLineHeight className='w-6 h-6 mx-auto cursor-pointer' />
-              <p className="text-center my-1.5">{item?.height} m</p>
-            </div>
+              <div className="grid grid-cols-3 p-2 gap-2">
+                <div className="mx-auto text-xs w-20">
+                  <MdApartment className='w-6 h-6 mx-auto'/>
+                  <p className="text-center my-1.5">{item?.floors} Floors</p>
+                </div>
 
-            <div className="mx-auto text-xs p-1  w-20 border-r border-l border-gray-400">
-              <AiOutlineColumnWidth className='w-6 h-6 mx-auto cursor-pointer'/>
-              <p className="text-center my-1.5">{item?.width} m</p>
-            </div>
+                <div className="mx-auto text-xs w-20 border-r border-l border-gray-400">
+                  <MdBedroomChild className='w-6 h-6 mx-auto'/>
+                  <p className="text-center my-1.5">{item?.bedroom} Bedrooms</p>
+                </div>
 
-            <div className="mx-auto text-xs p-1  w-20">
-              <PiMapPinAreaDuotone className='w-6 h-6 mx-auto cursor-pointer'/>
-              <p className="text-center my-1.5">{item?.area} Area</p>
+                <div className="mx-auto text-xs w-20">
+                  <MdOutlineBathroom className='w-6 h-6 mx-auto'/>
+                  <p className="text-center my-1.5">{item?.bathroom} Bathrooms</p>
+                </div>
+
+                <div className="mx-auto text-xs w-20">
+                  <CiLineHeight className='w-6 h-6 mx-auto' />
+                  <p className="text-center my-1.5">{item?.height} m</p>
+                </div>
+
+                <div className="mx-auto text-xs w-20 border-r border-l border-gray-400">
+                  <AiOutlineColumnWidth className='w-6 h-6 mx-auto'/>
+                  <p className="text-center my-1.5">{item?.width} m</p>
+                </div>
+
+                <div className="mx-auto text-xs w-20">
+                  <PiMapPinAreaDuotone className='w-6 h-6 mx-auto'/>
+                  <p className="text-center my-1.5">{item?.area}</p>
+                </div>
+              </div>
+
             </div>
-          </div>
+          ))}
         </div>
-      ))}
-    </div>
       </div>
 
-      <div className='mx-auto'>
+      {/* Pagination */}
+      <div className='mt-6'>
         <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
         />
       </div>
     </div>

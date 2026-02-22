e8c3f78e4b7a99788a7455de398005ebd84bd81c
chricmako6 <156549010+chricmako6@users.noreply.github.com>
2026-02-22 18:54:07 -0500

    refactor shop page layout for improved responsiveness and user experience; enhance product details display

--- app/shop/[id]/page.jsx
+++ app/shop/[id]/page.jsx
@@ -40,202 +40,204 @@ function page() {
   return (
     <div className='w-full'>
         <Navbar />
-        <div className='flex flex-wrap gap-4 max-w-7xl my-15 mx-auto p-6 rounded-md bg-white shadow-md'>
-           {/* clickable thumbnails */}
-            <div className='flex-3 flex flex-col gap-3'>
-              {[product.image, product.image, product.image].map((img, index) => (
-                <img
-                  key={index}
-                  src={img}
-                  alt=""
-                  onClick={() => setActiveImage(img)}
-                  className={`w-24 h-24 mt-5 rounded-xl mx-auto cursor-pointer border-2 transition-all
-                    ${activeImage === img ? "border-[#ffd061]" : "border-transparent"}
-                    hover:opacity-80 bg-cover shadow-xl`}
-                />
-              ))}
-            </div>
+        <div className="md:p-0 p-4">
+          <div className='flex flex-col lg:flex-row gap-4 max-w-7xl my-15 mx-auto md:p-6 p-4 rounded-md bg-white shadow-md'>
+            {/* clickable thumbnails */}
+              <div className='flex flex-row lg:flex-col gap-3'>
+                {[product.image, product.image, product.image].map((img, index) => (
+                  <img
+                    key={index}
+                    src={img}
+                    alt=""
+                    onClick={() => setActiveImage(img)}
+                    className={`w-24 h-24 mt-5 rounded-xl mx-auto cursor-pointer border-2 transition-all
+                      ${activeImage === img ? "border-[#ffd061]" : "border-transparent"}
+                      hover:opacity-80 bg-cover shadow-xl`}
+                  />
+                ))}
+              </div>
 
-            {/* this is the center image */}
-            <div className='flex-10 p-2'>
               {/* this is the center image */}
               <div className='flex-10 p-2'>
-                <div className="relative rounded-xl">
+                {/* this is the center image */}
+                <div className='flex-10 p-2'>
+                  <div className="relative rounded-xl">
 
-                  {/* BUTTONS */}
-                  <div className="absolute flex top-44 justify-between w-full p-5">
-                    {/* PREV BUTTON */}
-                    <button className="bg-[#ffd061] hover:bg-[#f5c84a] cursor-pointer rounded-full p-2">
-                      <BiArrowToTop className="w-5 h-5 -rotate-90"/>
-                    </button>
+                    {/* BUTTONS */}
+                    <div className="absolute flex top-44 justify-between w-full p-5">
+                      {/* PREV BUTTON */}
+                      <button className="bg-[#ffd061] hover:bg-[#f5c84a] cursor-pointer rounded-full p-2">
+                        <BiArrowToTop className="w-5 h-5 -rotate-90"/>
+                      </button>
 
-                    {/* NEXT BUTTON */}
-                    <div>
-                    <button className="bg-[#ffd061] hover:bg-[#f5c84a] cursor-pointer rounded-full p-2">
-                      <BiArrowToTop className="w-5 h-5 rotate-90"/>
-                    </button>
+                      {/* NEXT BUTTON */}
+                      <div>
+                      <button className="bg-[#ffd061] hover:bg-[#f5c84a] cursor-pointer rounded-full p-2">
+                        <BiArrowToTop className="w-5 h-5 rotate-90"/>
+                      </button>
+                      </div>
                     </div>
+                    
+                    <AnimatePresence mode="wait">
+                      <motion.img
+                        key={activeImage}
+                        src={activeImage}
+                        alt="image"
+                        initial={{ opacity: 0, scale: 0.95 }}
+                        animate={{ opacity: 1, scale: 1 }}
+                        exit={{ opacity: 0, scale: 0.95 }}
+                        transition={{ duration: 0.35, ease: "easeOut" }}
+                        className='w-full lg:h-96 h-90 rounded-xl shadow-xl object-cover bg-center'
+                      />
+                    </AnimatePresence>
+
                   </div>
-                  
-                  <AnimatePresence mode="wait">
-                    <motion.img
-                      key={activeImage}
-                      src={activeImage}
-                      alt="image"
-                      initial={{ opacity: 0, scale: 0.95 }}
-                      animate={{ opacity: 1, scale: 1 }}
-                      exit={{ opacity: 0, scale: 0.95 }}
-                      transition={{ duration: 0.35, ease: "easeOut" }}
-                      className='w-full h-96 rounded-xl shadow-xl object-cover bg-center'
-                    />
-                  </AnimatePresence>
+                </div>
 
+                <div className="grid grid-cols-2 lg:grid-cols-3 p-2 gap-4 mt-5">
+                  <div className="mx-auto text-md p-1 w-full lg:w-40">
+                    <MdApartment className='w-8 h-8 mx-auto cursor-pointer'/>
+                    <p className="text-center my-1.5">{product?.floors} Floors</p>
+                  </div>
+      
+                  <div className="mx-auto text-md p-1  w-40 lg:border-r-2 border-l-2 border-gray-300">
+                    <MdBedroomChild className='w-8 h-8 mx-auto cursor-pointer'/>
+                    <p className="text-center my-1.5">{product?.bedroom} Bedrooms</p>
+                  </div>
+      
+                  <div className="mx-auto text-md p-1  w-40">
+                    <MdOutlineBathroom className='w-8 h-8 mx-auto cursor-pointer'/>
+                    <p className="text-center my-1.5">{product?.bathroom} Bathrooms</p>
+                  </div>
+      
+                    <div className="mx-auto w-40 text-md p-1 ">
+                    <CiLineHeight className='w-8 h-8 mx-auto cursor-pointer' />
+                    <p className="text-center my-1.5">{product?.height} m</p>
+                  </div>
+      
+                  <div className="mx-auto text-md p-1  w-40 border-r-2 lg:border-l-2 border-gray-300">
+                    <AiOutlineColumnWidth className='w-8 h-8 mx-auto cursor-pointer'/>
+                    <p className="text-center my-1.5">{product?.width} m</p>
+                  </div>
+      
+                  <div className="mx-auto text-md p-1  w-40">
+                    <PiMapPinAreaDuotone className='w-8 h-8 mx-auto font-bold cursor-pointer'/>
+                    <p className="text-center my-1.5">{product?.area} Area</p>
+                  </div>
                 </div>
               </div>
 
-              <div className="grid grid-cols-3 p-2 gap-2 mt-5">
-                <div className="mx-auto text-md p-1 w-40">
-                  <MdApartment className='w-8 h-8 mx-auto cursor-pointer'/>
-                  <p className="text-center my-1.5">{product?.floors} Floors</p>
-                </div>
-    
-                <div className="mx-auto text-md p-1  w-40 border-r-2 border-l-2 border-gray-300">
-                  <MdBedroomChild className='w-8 h-8 mx-auto cursor-pointer'/>
-                  <p className="text-center my-1.5">{product?.bedroom} Bedrooms</p>
-                </div>
-    
-                <div className="mx-auto text-md p-1  w-40">
-                  <MdOutlineBathroom className='w-8 h-8 mx-auto cursor-pointer'/>
-                  <p className="text-center my-1.5">{product?.bathroom} Bathrooms</p>
-                </div>
-    
-                  <div className="mx-auto w-40 text-md p-1 ">
-                  <CiLineHeight className='w-8 h-8 mx-auto cursor-pointer' />
-                  <p className="text-center my-1.5">{product?.height} m</p>
-                </div>
-    
-                <div className="mx-auto text-md p-1  w-40 border-r-2 border-l-2 border-gray-300">
-                  <AiOutlineColumnWidth className='w-8 h-8 mx-auto cursor-pointer'/>
-                  <p className="text-center my-1.5">{product?.width} m</p>
-                </div>
-    
-                <div className="mx-auto text-md p-1  w-40">
-                  <PiMapPinAreaDuotone className='w-8 h-8 mx-auto font-bold cursor-pointer'/>
-                  <p className="text-center my-1.5">{product?.area} Area</p>
-                </div>
-              </div>
-            </div>
+              <div className='flex-7 mx-5'>
+                <h1 className='my-5 text-2xl font-bold'>
+                  {product?.title} {product?.id}
+                </h1>
 
-            <div className='flex-7 mx-5'>
-               <h1 className='my-5 text-2xl font-bold'>
-                {product?.title} {product?.id}
-               </h1>
-
-               {/* this is with radio */}
-               <div className="p-2 border rounded-xl border-gray-300">
-                <h1>File Type</h1>
-                <span className='gap-2 flex'>
-                    <input type="radio" name="Rooms" id="" className='cursor-pointer'/>
-                    <p className="text-gray-700 leading-relaxed text-sm">CAD + PDF</p>
-                </span>
-                <span className='gap-2 flex'>
-                    <input type="radio" name="Rooms" id="" className='cursor-pointer'/>
-                    <p className="text-gray-700 leading-relaxed text-sm">PDF</p>
-                </span>
-               </div>
-
-                {/* this is with checkbox */}
-               <div className="p-2 md:my-6 my-3 border rounded-xl border-gray-300">
-                <h1>Drawing Sets</h1>
-                <span className='gap-2 flex'>
-                    <input type="checkbox" name="Rooms" id="" className='cursor-pointer'/>
-                    <p className="text-gray-700 leading-relaxed text-sm">Architectural Drawings</p>
-                </span>
-               </div>
-
-               {/* THIS IS FOR DROPDOWN */}
-                  <span 
-                  className="overflow-hidden">
-                  <h2 className="font-bold my-2">Description</h2>
+                {/* this is with radio */}
+                <div className="p-2 border rounded-xl border-gray-300">
+                  <h1>File Type</h1>
                   <span className='gap-2 flex'>
-                      <p className="text-gray-700 leading-relaxed text-sm">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Obcaecati ad veritatis ea iste eligendi animi quis facere ducimus asperiores, 
-                        facilis ex repellat doloribus rerum harum?
-                      </p>
+                      <input type="radio" name="Rooms" id="" className='cursor-pointer'/>
+                      <p className="text-gray-700 leading-relaxed text-sm">CAD + PDF</p>
                   </span>
+                  <span className='gap-2 flex'>
+                      <input type="radio" name="Rooms" id="" className='cursor-pointer'/>
+                      <p className="text-gray-700 leading-relaxed text-sm">PDF</p>
+                  </span>
+                </div>
+
+                  {/* this is with checkbox */}
+                <div className="p-2 md:my-6 my-3 border rounded-xl border-gray-300">
+                  <h1>Drawing Sets</h1>
+                  <span className='gap-2 flex'>
+                      <input type="checkbox" name="Rooms" id="" className='cursor-pointer'/>
+                      <p className="text-gray-700 leading-relaxed text-sm">Architectural Drawings</p>
+                  </span>
+                </div>
+
+                {/* THIS IS FOR DROPDOWN */}
+                    <span 
+                    className="overflow-hidden">
+                    <h2 className="font-bold my-2">Description</h2>
+                    <span className='gap-2 flex'>
+                        <p className="text-gray-700 leading-relaxed text-sm">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                          Obcaecati ad veritatis ea iste eligendi animi quis facere ducimus asperiores, 
+                          facilis ex repellat doloribus rerum harum?
+                        </p>
+                    </span>
+                    </span>
+
+                {/* delivery information */}
+                <div className="p-2 my-3">
+                  <span className='gap-2 flex'>
+                      {/* icons */}
+                      <CiDeliveryTruck className='w-7 h-7 font-bold'/>
+                      <p className="text-gray-700 leading-relaxed text-sm">Instant digital delivery</p>
+                  </span>
+                  <span className='gap-2 flex my-2.5'>
+                      {/* icons */}
+                      <GiTakeMyMoney className='w-7 h-7 font-bold'/>
+                      <p className="text-gray-700 leading-relaxed text-sm">100% Money Guarantee</p>
+                  </span>
+                  <span className='gap-2 flex'>
+                      {/* icons */}
+                      <GiMoneyStack className='w-7 h-7 font-bold' />
+                      <p className="text-gray-700 leading-relaxed text-sm">Multiple payment options</p>
                   </span>
 
-               {/* delivery information */}
-               <div className="p-2 my-3">
-                <span className='gap-2 flex'>
-                    {/* icons */}
-                    <CiDeliveryTruck className='w-7 h-7 font-bold'/>
-                    <p className="text-gray-700 leading-relaxed text-sm">Instant digital delivery</p>
-                </span>
-                <span className='gap-2 flex my-2.5'>
-                    {/* icons */}
-                    <GiTakeMyMoney className='w-7 h-7 font-bold'/>
-                    <p className="text-gray-700 leading-relaxed text-sm">100% Money Guarantee</p>
-                </span>
-                <span className='gap-2 flex'>
-                    {/* icons */}
-                    <GiMoneyStack className='w-7 h-7 font-bold' />
-                    <p className="text-gray-700 leading-relaxed text-sm">Multiple payment options</p>
-                </span>
+                  <div className="flex gap-4 my-3">
+                    {/* BUY NOW */}
+                    <div className='w-full'>
+                      <Link href="/checkout">
+                      <button
+                      onClick={() => toast.success("Proceeding to checkout")}
+                      className='my-3 p-2 w-full rounded-xl shadow-md cursor-pointer text-gray-100 bg-[#ffd061] hover:bg-[#f5c84a]'>
+                        <h1 className='font-bold'>
+                          Buy {product?.price}
+                        </h1>
+                      </button>
+                      </Link>
+                    </div>
 
-                <div className="flex gap-4 my-3">
-                  {/* BUY NOW */}
-                  <div className='w-full'>
-                    <Link href="/checkout">
-                    <button
-                     onClick={() => toast.success("Proceeding to checkout")}
-                     className='my-3 p-2 w-full rounded-xl shadow-md cursor-pointer text-gray-100 bg-[#ffd061] hover:bg-[#f5c84a]'>
-                      <h1 className='font-bold'>
-                        Buy {product?.price}
-                      </h1>
-                    </button>
-                    </Link>
+                    {/* ADD TO CART */}
+                    <div
+                    className='w-full'>
+                      <button
+                      onClick={() => toast.success("Added to Cart")}
+                      className='flex justify-center my-3 p-2 w-full rounded-xl shadow-md cursor-pointer text-gray-100 bg-[#ffd061] hover:bg-[#f5c84a]'>
+                        <h1 className='font-bold '>
+                          Add Cart
+                        </h1>
+                        <span className='px-1'>
+                          <TiShoppingCart className="text-lg w-5 h-5"/>
+                        </span>
+                      </button>
+                    </div>  
                   </div>
 
-                  {/* ADD TO CART */}
-                  <div
-                  className='w-full'>
-                    <button
-                    onClick={() => toast.success("Added to Cart")}
-                     className='flex justify-center my-3 p-2 w-full rounded-xl shadow-md cursor-pointer text-gray-100 bg-[#ffd061] hover:bg-[#f5c84a]'>
-                      <h1 className='font-bold '>
-                        Add Cart
-                      </h1>
-                      <span className='px-1'>
-                        <TiShoppingCart className="text-lg w-5 h-5"/>
-                      </span>
-                    </button>
-                  </div>  
-                </div>
+                  {/* add to favorite and descriptions */}
+                  <div className="">
+                    <span className="gap-2 flex my-5 cursor-pointer select-none">
+                      {isFavorite ? (
+                        <FaHeart
+                          onClick={toggleFavorite}
+                          className="w-6 h-6 text-red-600 transition-colors duration-300"
+                        />
+                      ) : (
+                        <FaRegHeart
+                          onClick={toggleFavorite}
+                          className="w-6 h-6 text-gray-700 transition-colors duration-300"
+                        />
+                      )}
+                      <p className="text-gray-700 leading-relaxed text-md">
+                        Favorite
+                      </p>
+                    </span>
 
-                {/* add to favorite and descriptions */}
-                <div className="">
-                  <span className="gap-2 flex my-5 cursor-pointer select-none">
-                    {isFavorite ? (
-                      <FaHeart
-                        onClick={toggleFavorite}
-                        className="w-6 h-6 text-red-600 transition-colors duration-300"
-                      />
-                    ) : (
-                      <FaRegHeart
-                        onClick={toggleFavorite}
-                        className="w-6 h-6 text-gray-700 transition-colors duration-300"
-                      />
-                    )}
-                    <p className="text-gray-700 leading-relaxed text-md">
-                      Favorite
-                    </p>
-                  </span>
-
+                  </div>
                 </div>
-               </div>
-            </div>
+              </div>
+          </div>
         </div>
 
         {/* this is for the footer */}

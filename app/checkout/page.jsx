4e2c4fb0c6357817b863567711e7550feed66a26
chricmako6 <christophersitta06@gmail.com>
2026-02-27 18:03:17 -0500

    refactor: enhance checkout page functionality; implement order summary and improve user interaction

--- app/checkout/page.jsx
+++ app/checkout/page.jsx
@@ -1,13 +1,19 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, useContext} from 'react'
+import Link from 'next/link';
 import { CgProfile } from "react-icons/cg";
 import { RiSecurePaymentLine } from "react-icons/ri";
 import { FaCheck } from "react-icons/fa6";
 import { FaCreditCard } from "react-icons/fa";
 import Footer from '@/component/01/footer'
 import Navbar from '@/component/01/navbar'
+import { StoreContext } from '../hooks/context/StoreContext';
+import { useRouter } from 'next/navigation';
 
 function PageCheck() {
+  const { products, clearCart } = useContext(StoreContext);
+  const router = useRouter();
+  const [orderId, setOrderId] = useState(null);
   const [selectedInfo, setSelectedInfo] = useState('person');
   const steps = ['person', 'payment', 'complete'];
 
@@ -25,32 +31,43 @@ function PageCheck() {
     }
   };
 
-  const informations = {
-    person : {
+  const generateOrderId = () => {
+    const random = Math.floor(100000 + Math.random() * 900000);
+    return `ORD-${random}`;
+  };
 
-    },
-    payment : {
-
-    },
-    complete : {
-
+  const getNumericPrice = (price) => {
+    if (typeof price === "string") {
+      const numericValue = parseFloat(price.replace(/[^\d.]/g, ""));
+      return isNaN(numericValue) ? 0 : numericValue;
     }
-  }
+    return typeof price === "number" ? price : 0;
+  };
+
+  // Subtotal
+  const subtotal = products.reduce((acc, item) => {
+    const numericPrice = getNumericPrice(item.price);
+    const itemTotal = numericPrice * (item.quantity || 1);
+    return acc + itemTotal;
+  }, 0);
+
+  const deliveryCost = 20;
+  const tax = 5;
+  const discount = subtotal * 0.1;
+  const finalTotal = subtotal + deliveryCost + tax - discount;
 
   // Handle information click
   const handleInfoClick = (information) => {
     setSelectedInfo(information);
   };
 
-  // Get currently selected information
-  const currentInfo = informations[selectedInfo];
 
   return (
     <div className='max-w-full'>
       <Navbar />
       <div className="p-4">
         <div className="bg-white my-20 rounded-lg  shadow-md p-4 md:max-w-5xl mx-auto">
-          <div className='bg-gray-100 max-w-4xl mx-auto p-2 rounded-full flex justify-between'>
+          <div className='bg-gray-100 border border-gray-200 max-w-4xl mx-auto p-2 rounded-full flex justify-between'>
             {/* PERSONAL */}
             <button
               onClick={() => setSelectedInfo('person')}
@@ -90,14 +107,143 @@ function PageCheck() {
 
 
           {/* THIS WHICH HAVE CARRY THE LAYOUT */}
-          <div className="max-w-4xl mx-auto mt-8 p-6 bg-gray-50 rounded-lg shadow-md">
-
+          <div className="max-w-4xl mx-auto mt-8 p-6 bg-gray-100 border border-gray-200 rounded-lg shadow-md">
+            {/* THIS IS FOR PERSON */}
             {selectedInfo === 'person' && (
-              <div>
-              
+              <div className="flex gap-4">
+                
+                {/* PERSONAL INFORMATION FORM */}
+                <div className="bg-white border border-gray-200 flex-10 p-4 rounded shadow-xs">
+                  <h1 className="font-semibold">
+                    Personal Information
+                  </h1>
+                  <p className="text-sm text-gray-400 mt-2">
+                    Please provide your details for delivery and contact.
+                  </p>
+
+                  <div className="mt-4 space-y-4">
+
+                    {/* FULL NAME */}
+                    <div>
+                      <p className="text-gray-700 leading-relaxed text-xs mb-1">
+                        Full Name
+                      </p>
+                      <input
+                        type="text"
+                        placeholder="Enter your full name"
+                        className="w-full text-sm border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#ffd061]"
+                      />
+                    </div>
+
+                    {/* EMAIL */}
+                    <div>
+                      <p className="text-gray-700 leading-relaxed text-xs mb-1">
+                        Email Address
+                      </p>
+                      <input
+                        type="email"
+                        placeholder="Enter your email"
+                        className="w-full text-sm border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#ffd061]"
+                      />
+                    </div>
+
+                    {/* PHONE */}
+                    <div>
+                      <p className="text-gray-700 leading-relaxed text-xs mb-1">
+                        Phone Number
+                      </p>
+                      <input
+                        type="tel"
+                        placeholder="Enter your phone number"
+                        className="w-full text-sm border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#ffd061]"
+                      />
+                    </div>
+
+                    {/* LOCATION */}
+                    <div>
+                      <p className="text-gray-700 leading-relaxed text-xs mb-1">
+                        Location
+                      </p>
+                      <input
+                        type="text"
+                        placeholder="City / Region"
+                        className="w-full text-sm border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#ffd061]"
+                      />
+                    </div>
+
+                    {/* DESCRIPTION */}
+                    <div>
+                      <p className="text-gray-700 leading-relaxed text-xs mb-1">
+                        Additional Notes
+                      </p>
+                      <textarea
+                        rows="4"
+                        placeholder="Write any delivery instructions..."
+                        className="w-full text-sm border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-[#ffd061]"
+                      />
+                    </div>
+
+                    <button 
+                    onClick={() => {
+                      setSelectedInfo("payment");
+                    }}
+                    className="my-3 p-2 w-full rounded-xl shadow-md cursor-pointer bg-[#ffd061] hover:bg-[#f5c84a]">
+                      Save & Continue
+                    </button>
+
+                  </div>
+                </div>
+
+                {/* ORDER SUMMARY  */}
+                <div className="bg-white border border-gray-200 flex-7 p-4 h-fit rounded shadow-xs">
+                  <h1 className="font-bold border-b border-gray-200 p-3">
+                    Your Cart (<span>{products.length}</span>)
+                  </h1>
+
+                  <div className="my-4 bg-white border border-gray-200 p-3 rounded-md">
+                    <h1 className="leading-relaxed font-bold text-[15px]">
+                      Order summary
+                    </h1>
+
+                    <div className="flex justify-between items-center py-1.5">
+                      <p className="text-sm text-gray-400">Subtotal</p>
+                      <span className="text-sm text-gray-600">
+                        Tshs: {subtotal.toFixed(2)}
+                      </span>
+                    </div>
+
+                    <div className="flex justify-between items-center py-1">
+                      <p className="text-sm text-gray-400">Delivery cost</p>
+                      <span className="text-sm text-gray-600">
+                        Tshs: {deliveryCost}
+                      </span>
+                    </div>
+
+                    <div className="flex justify-between items-center py-1">
+                      <p className="text-sm text-gray-400">Tax</p>
+                      <span className="text-sm text-gray-600">
+                        Tshs: {tax}
+                      </span>
+                    </div>
+
+                    <div className="flex justify-between items-center py-1">
+                      <p className="text-sm text-gray-400">Discount</p>
+                      <span className="text-sm text-gray-600">10%</span>
+                    </div>
+
+                    <div className="flex border-t border-gray-200 justify-between items-center py-2.5 mt-3">
+                      <p className="text-sm text-gray-400">Total</p>
+                      <span className="text-sm text-gray-600">
+                        Tshs: {finalTotal.toFixed(2)}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+
               </div>
             )}
 
+            {/* THIS IS FOR PAYMENT */}
             {selectedInfo === 'payment' && (
               <div className="flex  gap-4">
                 {/* THIS IS FOR PAYMENT OPTIONS */}
@@ -168,20 +314,158 @@ function PageCheck() {
                       <p className="text-gray-700 leading-relaxed text-sm">Cash on delivery</p>
                     </span>
 
-                    <button className="my-3 p-2 w-full rounded-xl shadow-md cursor-pointer bg-[#ffd061] hover:bg-[#f5c84a]">
-                      Pay | $100
+                    <button 
+                      onClick={() => {
+                        const id = generateOrderId();
+                        setOrderId(id);
+                        setSelectedInfo("complete");
+
+                        setTimeout(() => {
+                          clearCart();
+                          router.push("/");
+                        }, 60000);
+                      }}
+                    className="my-3 p-2 w-full rounded-xl shadow-md cursor-pointer bg-[#ffd061] hover:bg-[#f5c84a]">
+                      Pay | {finalTotal.toFixed(2)}
                     </button>
                   </div>
                 </div>
 
                 {/* THIS IS FOR PAYMENT SUMMARY */}
-                <div className="bg-white flex-7 p-4 rounded shadow-xs">2</div>
+                <div className="bg-white border border-gray-200 flex-7 p-4 h-fit rounded shadow-xs">
+                  <h1 className="font-bold border-b border-gray-200 p-3">
+                    Your Cart (<span className=''>{products.length}</span>)
+                  </h1>
+
+                  <div className="my-4 bg-white border border-gray-200 p-3 rounded-md">
+                    <h1 className="leading-relaxed font-bold text-[15px]">Order summary</h1>
+                    <div className="flex justify-between items-center py-1.5">
+                      <p className="text-sm text-gray-400">Subtotal</p>
+                      <span className="text-sm text-gray-600">Tshs: {subtotal.toFixed(2)}</span>
+                    </div>
+                    <div className="flex justify-between items-center py-1">
+                      <p className="text-sm text-gray-400">Delivery cost</p>
+                      <span className="text-sm text-gray-600">Tshs: {deliveryCost}</span>
+                    </div>
+                    <div className="flex justify-between items-center py-1">
+                      <p className="text-sm text-gray-400">Tax</p>
+                      <span className="text-sm text-gray-600">Tshs: {tax}</span>
+                    </div>
+                    <div className="flex justify-between items-center py-1">
+                      <p className="text-sm text-gray-400">Discount</p>
+                      <span className="text-sm text-gray-600">10%</span>
+                    </div>
+
+                    {/* THIS IS FOR TOTAL */}
+                    <div className="flex border-t border-gray-200 justify-between items-center py-2.5 mt-3">
+                      <p className="text-sm text-gray-400">Total</p>
+                      <span className="text-sm text-gray-600">Tshs: {finalTotal.toFixed(2)}</span>
+                    </div>
+                  </div>
+                </div>
               </div>
             )}
 
+            {/* THIS IS FOR COMPLETE */}
             {selectedInfo === 'complete' && (
-              <div className="text-center">
-              
+              <div className="flex gap-4">
+                
+                {/* SUCCESS MESSAGE */}
+                <div className="bg-white border border-gray-200 flex-10 p-6 rounded shadow-xs text-center">
+
+                  {/* SUCCESS ICON */}
+                  <div className="flex justify-center mb-4">
+                    <div className="bg-[#ffd061] p-4 rounded-full shadow-md">
+                      <FaCheck className="w-8 h-8 text-white" />
+                    </div>
+                  </div>
+
+                  <h1 className="text-2xl font-bold">
+                    Payment Successful
+                  </h1>
+
+                  <p className="text-gray-500 mt-2 text-sm">
+                    Thank you for your purchase. Your order has been successfully placed.
+                  </p>
+
+                  <p className="text-gray-400 mt-1 text-sm">
+                    A confirmation email has been sent with your order details.
+                  </p>
+
+                  <div className="mt-6 border-t border-gray-200 pt-4">
+                    <p className="text-sm text-gray-500">
+                      Estimated delivery time:
+                    </p>
+                    <p className="font-semibold text-gray-700">
+                      2 - 5 Business Days
+                    </p>
+                  </div>
+
+                  <div className="mt-6 flex justify-center gap-4">
+                    <button
+                      onClick={() => {
+                        clearCart()
+                      }}
+                      className="px-4 py-2 cursor-pointer rounded-lg border border-gray-300 hover:bg-gray-100 transition"
+                    >
+                      <Link href="/">
+                        Confirm Order
+                      </Link>
+                    </button>
+
+                    <button className="px-4 py-2 cursor-pointer rounded-lg bg-[#ffd061] hover:bg-[#f5c84a] transition shadow-md">
+                      Track Order
+                    </button>
+                  </div>
+                </div>
+
+                {/* FINAL ORDER SUMMARY */}
+                <div className="bg-white border border-gray-200 flex-7 p-4 h-fit rounded shadow-xs">
+                  <h1 className="font-bold border-b border-gray-200 p-3">
+                    Order Summary
+                  </h1>
+
+                  <div className="my-4 bg-white border border-gray-200 p-3 rounded-md">
+                    <h1 className="text-sm text-gray-400 flex justify-between items-center mt-2">
+                      <p>Order ID:</p> <span className="font-semibold text-gray-600">{orderId}</span>
+                    </h1>
+                    <div className="flex justify-between items-center py-1.5">
+                      <p className="text-sm text-gray-400">Items</p>
+                      <span className="text-sm text-gray-600">
+                        {products.length}
+                      </span>
+                    </div>
+
+                    <div className="flex justify-between items-center py-1.5">
+                      <p className="text-sm text-gray-400">Subtotal</p>
+                      <span className="text-sm text-gray-600">
+                        Tshs: {subtotal.toFixed(2)}
+                      </span>
+                    </div>
+
+                    <div className="flex justify-between items-center py-1">
+                      <p className="text-sm text-gray-400">Delivery</p>
+                      <span className="text-sm text-gray-600">
+                        Tshs: {deliveryCost}
+                      </span>
+                    </div>
+
+                    <div className="flex justify-between items-center py-1">
+                      <p className="text-sm text-gray-400">Tax</p>
+                      <span className="text-sm text-gray-600">
+                        Tshs: {tax}
+                      </span>
+                    </div>
+
+                    <div className="flex border-t border-gray-200 justify-between items-center py-2.5 mt-3 font-bold">
+                      <p>Total Paid</p>
+                      <span>
+                        Tshs: {finalTotal.toFixed(2)}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+
               </div>
             )}
 
@@ -190,7 +474,7 @@ function PageCheck() {
               {currentStepIndex > 0 && (
                 <button
                   onClick={goBack}
-                  className="bg-gray-300 px-4 py-2 rounded hover:bg-gray-400"
+                  className="bg-gray-300 cursor-pointer px-4 py-2 rounded hover:bg-gray-400"
                 >
                   Back
                 </button>
@@ -199,7 +483,7 @@ function PageCheck() {
               {currentStepIndex < steps.length - 1 && (
                 <button
                   onClick={goNext}
-                  className="bg-[#ffd061] px-4 py-2 rounded hover:bg-[#f5c84a]"
+                  className="bg-[#ffd061] cursor-pointer px-4 py-2 rounded hover:bg-[#f5c84a]"
                 >
                   Next
                 </button>
